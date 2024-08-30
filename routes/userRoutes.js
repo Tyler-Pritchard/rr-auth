08275c0d569974c8f272fddafcc93fe34e6e990d
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-08-30 13:23:53 -0700

    add login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
 const router = express.Router();
 
 const SECRET_KEY = process.env.RECAPTCHA_SECRET_KEY;
+
 
 // @route   POST /api/users/register
 // @desc    Register a new user
@@ -46,5 +49,48 @@ router.post('/register', async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+
+// @route   POST /api/users/login
+// @desc    Authenticate user and get token
+// @access  Public
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+  
+    try {
+      // Check for the user
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+  
+      // Validate password
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ msg: 'Invalid Credentials' });
+      }
+  
+      // Return JWT
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+  
+      jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        { expiresIn: '1h' },  // Token expires in 1 hour
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  });
+  
 
 module.exports = router;
