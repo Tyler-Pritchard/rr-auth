8666122dee2317fb69d355947d008aba6e5dede0
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-09-03 11:11:25 -0700

    add prod google creds

--- server.js
+++ server.js
@@ -3,19 +3,31 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
-const cors = require('cors');  // Add this line to import the cors module
+const fs = require('fs');
+const path = require('path');
+const cors = require('cors');
 
 // Routes
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 
+// Decode Base64 string to JSON
+const base64Credentials = process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64;
+const jsonCredentials = Buffer.from(base64Credentials, 'base64').toString('utf8');
+
+// Write JSON to a temporary file
+const tempFilePath = path.join(__dirname, 'gcloud-credentials.json');
+fs.writeFileSync(tempFilePath, jsonCredentials);
+
+// Set the environment variable to the path of the temporary file
+process.env.GOOGLE_APPLICATION_CREDENTIALS = tempFilePath;
+
 // check for google credentials in .env
 if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
   console.error("ERROR: The GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.");
   process.exit(1);
 }
-
 
 const app = express();
 
@@ -25,7 +37,7 @@ app.set('trust proxy', 1);
 // Enable CORS for requests from http://localhost:3000
 app.use(cors({
   origin: 'http://localhost:3000',
-  credentials: true, // Include this option if your frontend needs to send cookies or other credentials
+  credentials: true,
 }));
 
 app.use(express.json());
@@ -34,7 +46,6 @@ app.use(
     directives: {
       defaultSrc: ["'self'"],
       scriptSrc: ["'self'", "'unsafe-inline'", "https://www.google.com", "https://www.gstatic.com"],
-      // other directives...
     },
   })
 );
