f3e329da860762096c123c28f6172d5249658806
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-09-03 22:38:48 -0700

    temp disable captcha

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -57,9 +57,9 @@ router.post('/register', authLimiter, async (req, res) => {
     // Verify CAPTCHA
     const recaptchaScore = await verifyRecaptchaToken(captchaToken);
 
-    if (recaptchaScore === null || recaptchaScore < 0.5) {
-      return res.status(400).json({ msg: 'CAPTCHA verification failed' });
-    }
+    // if (recaptchaScore === null || recaptchaScore < 0.5) {
+    //   return res.status(400).json({ msg: 'CAPTCHA verification failed' });
+    // }
 
     // Check if the user already exists
     let user = await User.findOne({ email });
