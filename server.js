7ec1aa813dd5d26095bcf46e8344f25ccf7e3ba6
Tyler Pritchard <pritchard.tyler@gmail.com>
2024-12-11 22:45:48 -0800

    prod cors config

--- server.js
+++ server.js
@@ -108,17 +108,18 @@ app.use(cors({
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  methods: allowedMethods,
+  allowedHeaders: allowedHeaders,
 }));
 
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.sendStatus(204); // Respond with no content for OPTIONS
-});
+// app.options('*', (req, res) => {
+//   res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.sendStatus(204); // Respond with no content for OPTIONS
+// });
+app.options('*', cors()); // Handle preflight requests
 
 /**
  * Middleware: Preflight Request Handler
